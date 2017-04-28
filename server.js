94fc53edf58f62add9a187e59477ca058a56ebbb
prog4life <prog4life@users.noreply.github.com>
2017-04-28 17:02:33 +0300

    Removed IP and localhost set from server.js

--- server.js
+++ server.js
@@ -13,9 +13,11 @@ app.use(function (req, res, next) {
 
 app.use(express.static('public'));
 
-const IP = process.env.IP || 'localhost';
+// const IP = process.env.IP || 'localhost';
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, IP, () => {
-  console.log('Server is up on: ', IP, ':', PORT);
+// app.listen(PORT, IP, () => {
+app.listen(PORT, () => {
+  // console.log('Server is up on: ', IP, ':', PORT);
+  console.log('Server is up on port: ', PORT);
 });
