dd342ba8f61c1c5d41ce1e53e1ca80ed33225328
prog4life <prog4life@users.noreply.github.com>
2017-04-28 21:18:14 +0300

    server.js version suited for Heroku

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ var express = require('express');
 var app = express();
 
 app.use(function (req, res, next) {
+  console.log("req.headers['x-forwarded-proto']: ",
+  req.headers['x-forwarded-proto']);
+  console.log('req.protocol: ', req.protocol);
+  console.log('req.secure: ', req.secure);
+  console.log('req.hostname: ', req.hostname);
+  console.log('req.url: ', req.url);
+  console.log('req.originalUrl: ', req.originalUrl);
+
   if (req.headers['x-forwarded-proto'] === 'http') {
-  // if (req.protocol === 'http') {
+  // get wrong 'http' value of req.protocol in Heroku, while 'https' is expected
+  // if (req.protocol === 'http') {  // for 'localhost'
     next();
   } else {
-    res.redirect('http://' + req.hostname + req.url);
-    // res.redirect('http://' + req.hostname + req.originalUrl);
+    res.redirect('http://' + req.hostname + req.originalUrl);  // or req.url
+    // res.redirect('http://' + req.hostname + ':' +
+    // (process.env.PORT || 3000) + req.url);  // for 'localhost'
   }
 });
 
