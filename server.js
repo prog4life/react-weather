ed077952f40409f2064438a94c770838a4449853
prog4life <prog4life@users.noreply.github.com>
2017-04-28 15:56:56 +0300

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
 
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+  // if (req.protocol === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+    // res.redirect('http://' + req.hostname + req.originalUrl);
+  }
+});
+
 app.use(express.static('public'));
 
-var IP = process.env.IP || 'localhost';
-var PORT = process.env.PORT || 3000;
+const IP = process.env.IP || 'localhost';
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, IP, () => {
   console.log('Server is up on: ', IP, ':', PORT);
