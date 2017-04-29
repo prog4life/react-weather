b9c97276ad4dffd796591ebc24b2f7840e1f441a
prog4life <prog4life@users.noreply.github.com>
2017-04-29 15:09:25 +0300

    Update middleware in server.js to fix work with localhost

--- server.js
+++ server.js
@@ -2,22 +2,10 @@ var express = require('express');
 var app = express();
 
 app.use(function (req, res, next) {
-  console.log("req.headers['x-forwarded-proto']: ",
-  req.headers['x-forwarded-proto']);
-  console.log('req.protocol: ', req.protocol);
-  console.log('req.secure: ', req.secure);
-  console.log('req.hostname: ', req.hostname);
-  console.log('req.url: ', req.url);
-  console.log('req.originalUrl: ', req.originalUrl);
-
-  if (req.headers['x-forwarded-proto'] === 'http') {
-  // get wrong 'http' value of req.protocol in Heroku, while 'https' is expected
-  // if (req.protocol === 'http') {  // for 'localhost'
-    next();
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);  // or req.originalUrl
   } else {
-    res.redirect('http://' + req.hostname + req.originalUrl);  // or req.url
-    // res.redirect('http://' + req.hostname + ':' +
-    // (process.env.PORT || 3000) + req.url);  // for 'localhost'
+    next();
   }
 });
 
